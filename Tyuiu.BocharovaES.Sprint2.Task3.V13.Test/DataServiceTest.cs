19cc591b1a03731a67f2f2bba9560a6e34f03f78
Екатерина Бочарова <kati.bocharova.06@mail.ru>
2025-10-12 21:26:12 +0500

    Task3

---
+++ Tyuiu.BocharovaES.Sprint2.Task3.V13.Test/DataServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.BocharovaES.Sprint2.Task3.V13.Lib;
+namespace Tyuiu.BocharovaES.Sprint2.Task3.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = -7;
+            Assert.AreEqual(res, wait);
+        }
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0.75;
+            Assert.AreEqual(res, wait);
+        }
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = 11;
+            Assert.AreEqual(res, wait);
+        }
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -18;
+            double res = ds.Calculate(x);
+            double wait = 198.056;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
