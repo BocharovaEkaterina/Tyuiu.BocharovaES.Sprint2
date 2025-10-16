60e3da4736644f7528d62deb1cc55536fe78654c
Екатерина Бочарова <kati.bocharova.06@mail.ru>
2025-10-16 21:20:32 +0500

    Task7

---
+++ Tyuiu.BocharovaES.Sprint2.Task7.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BocharovaES.Sprint2.Task7.V5.Lib;
+namespace Tyuiu.BocharovaES.Sprint2.Task7.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            double x = -0.4;
+            double y = 0.4;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
